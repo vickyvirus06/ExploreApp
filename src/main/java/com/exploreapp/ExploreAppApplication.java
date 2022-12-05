704dba1e4f33d45bd3773ca03bc1ef94f006f56e
VickyVirus <vickyvirus1995@protonmail.com>
2022-12-05 09:55:02 +0530

    First Commit
    Init

---
+++ src/main/java/com/exploreapp/ExploreAppApplication.java
@@ -0,0 +1,13 @@
+package com.exploreapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploreAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExploreAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exploreapp/ExploreAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exploreapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExploreAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
